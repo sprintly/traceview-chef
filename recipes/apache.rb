2e833bea8c92de08fb257dba85e20589b69ad5fb
Joe Stump <joe@joestump.net>
2012-04-26 13:22:12 -0700

    Added Apache configuration.

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -1,3 +1,26 @@
 package "libapache2-mod-oboe" do
     action :install
 end
+
+tracing_mode = "always"
+sampling_rate = "300000"
+if node.key?("tracelytics")
+    tracing_mode = node["tracelytics"].fetch("tracing_mode", tracing_mode)
+    sampling_rate = node["tracelytics"].fetch("sampling_rate", sampling_rate)
+end
+
+template "/etc/apache2/mods-available/oboe.conf" do
+    source "etc/apache2/modes-available/oboe.conf.erb"
+    mode "0644"
+    owner "root"
+    group "root"
+    variables(
+        :tracing_mode => tracing_mode,
+        :sampling_rate => sampling_rate
+    )
+    action :create
+end
+
+service "apache2" do
+    action :reload
+end
