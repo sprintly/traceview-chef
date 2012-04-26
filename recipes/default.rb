316482dfe22da6329e4e17e165ad3c6f26e19cb9
Joe Stump <joe@joestump.net>
2012-04-26 13:45:12 -0700

    Added all of the Tracelytics config files, etc.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,12 @@
+# This recipe only sets up your /etc/tracelytics.conf. Everything else should
+# be included in your run list depending on your distribution or stack.
+# 
 # Use the recipes for your distribution/stack setup.
 # - tracelytics::apt for Ubuntu/Debian distributions
 # - tracelytics::apache for Apache instrumentation
 # - tracelytics::python for Python instrumentation
+
+# Be sure to set your access_key in an environment/role/node attribute.
+template "/etc/tracelytics.conf" do
+    source "etc/tracelytics.conf"
+end
