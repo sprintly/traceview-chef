986eb60365fcb8235e2cbe74883396613ba247ca
Joe Stump <joe@joestump.net>
2012-04-26 08:47:36 -0700

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures tracelytics"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "pip"
+depends "apt"
