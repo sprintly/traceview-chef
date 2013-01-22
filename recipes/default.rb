ff43651928be4a66b1b4e4a10a4426a780bf0b6f
Paul Thomas <paul.thomas@livingsocial.com>
2013-01-22 11:43:40 +0000

    first pass at centos support adds depends on yum & bumps version

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,19 +6,26 @@
 # - tracelytics::apache for Apache instrumentation
 # - tracelytics::python for Python instrumentation
 
-# For now, we assume you're on a Debian/Ubuntu system. If you are not
-# this recipe will fail horribly.
-include_recipe "tracelytics::apt"
+case node['platform']
+when "ubuntu", "debian"
+    include_recipe "tracelytics::apt"
+when "redhat", "centos"
+    include_recipe "tracelytics::yum"
+end
 
 # Be sure to set your access_key in an environment/role/node attribute.
 template "/etc/tracelytics.conf" do
     source "etc/tracelytics.conf.erb"
 end
 
-# Be sure to include the tracelytics::apt recipe before this in your
-# run list. Need to make this more agnostic on distribution, but
-# we don't use RedHat/CentOS and I'm lazy.
-%w{liboboe0 liboboe-dev tracelyzer}.each do |package_name|
+# Install the packages
+case node['platform']
+when "ubuntu", "debian"
+    packages = ["liboboe0", "liboboe-dev", "tracelyzer"]
+when "redhat", "centos"
+    packages = ["liboboe", "liboboe-devel", "tracelyzer"]
+end
+packages.each do |package_name|
     package package_name do
         action :install
     end
