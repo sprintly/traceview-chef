587f4bbe822912a60c922027e4f9f94564fc7a21
Paul Thomas <paul.thomas@livingsocial.com>
2013-01-22 12:32:25 +0000

    use same indent as original

--- recipes/yum.rb
+++ recipes/yum.rb
@@ -2,13 +2,13 @@ majorver = node['platform_version'].to_i.to_s
 arch = node['kernel']['machine']
 
 yum_key "RPM-GPG-KEY-tracelytics" do
-  url "https://yum.tracelytics.com/RPM-GPG-KEY-tracelytics"
-  action :add
+    url "https://yum.tracelytics.com/RPM-GPG-KEY-tracelytics"
+    action :add
 end
 
 yum_repository "tracelytics" do
-  url "http://yum.tracelytics.com/#{majorver}/#{arch}"
-  key "RPM-GPG-KEY-tracelytics"
-  description "Tracelytics repository"
-  action :add
+    url "http://yum.tracelytics.com/#{majorver}/#{arch}"
+    key "RPM-GPG-KEY-tracelytics"
+    description "Tracelytics repository"
+    action :add
 end
