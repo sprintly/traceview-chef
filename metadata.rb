25190b9a65324aeaf6d9aa6beeebfe7a0a04f6f1
Joe Stump <joe@joestump.net>
2014-04-03 17:30:45 -0700

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "joe@stu.mp"
 license          "All rights reserved"
 description      "Installs/Configures tracelytics"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 
 depends "python"
 depends "apt"
