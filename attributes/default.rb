a5723a651b436eba6fbae884cf36669155a90453
Joe Stump <joe@joestump.net>
2014-04-03 17:39:27 -0700

    Added an attributes file and updated the apache recipe.

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+node.default['traceview']['apache']['tracing_mode'] = 'always'
+node.default['traceview']['apache']['sampling_rate'] = 300000
--- recipes/apache2.rb
+++ recipes/apache2.rb
@@ -4,23 +4,11 @@ package "libapache2-mod-oboe" do
     action :install
 end
 
-tracing_mode = "always"
-sampling_rate = "300000"
-
-if node.key?("traceview")
-    tracing_mode = node["traceview"].fetch("tracing_mode", tracing_mode)
-    sampling_rate = node["traceview"].fetch("sampling_rate", sampling_rate)
-end
-
 template "/etc/apache2/mods-available/oboe.conf" do
     source "etc/apache2/mods-available/oboe.conf.erb"
     mode "0644"
     owner "root"
     group "root"
-    variables(
-        :tracing_mode => tracing_mode,
-        :sampling_rate => sampling_rate
-    )
     action :create
     notifies :reload, "service[apache2]"
 end
